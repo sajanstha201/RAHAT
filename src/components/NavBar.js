79f3d27a92baaf2fe70f9f6dc97848d97fbf8eae
sajanshrestha <sajanstha201.55@gmail.com>
2023-12-18 12:51:31 +0530

    changed some url thing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ function NavBar(){
     return(
         <div className="NavBar">
           <div style={{width:'10px'}}>
-          <Link to="/club">
+          <Link to="">
             <img src={rahat_logo} alt="rahat logo" onMouseOver={()=>setStyle(true)} onMouseLeave={()=>setStyle(false)}
              style={style_rahat_logo} className="logo">
             </img>
